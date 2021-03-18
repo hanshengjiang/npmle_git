bdaa732cb69d3a506b60141ed685b83e8dbaa88b
Hansheng <jianghs626@gmail.com>
2021-03-17 22:52:53 -0700

    Update run_simulation.py
    current em algorithm does not apply to nonlinear regression models

--- scripts/run_simulation.py
+++ scripts/run_simulation.py
@@ -8,7 +8,7 @@
 
 '''
 
-1. simulations of discrete mixtures
+1. simulations of discrete mixtures 
 2. density plots that compare EM, CGM, with or without knowing sigma
 
 '''
@@ -18,6 +18,7 @@ from package_import import *
 from simulation_lib import *
 from cv_procedure_lib import *
 from em_alg_lib import *
+from regression_func_lib import *
 import sys
 
 
@@ -26,8 +27,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 5:
         sigma = 0.5
         n = 500
-        config = '1'
-        run_cv = 0.5 
+        config = '4'
+        run_cv = '0.5'
     # otherwise take argyments from command line
     else:
         #sys_argv[0] is the name of the .py file
@@ -35,7 +36,8 @@ if __name__ == "__main__":
         n = int(sys.argv[2]) # number of data points
         config = sys.argv[3]
         run_cv = sys.argv[4]
-        
+
+# preset configurations      
 if config == '1':
     #----------- configuration 1-----#
     b1 = (1,1)
@@ -43,6 +45,9 @@ if config == '1':
     b3 = (-1,0.5)
     pi1 = 0.5
     pi2 = 0.5
+    func = lin_func
+    BL = -10
+    BR = 10
 elif config == '2':
     #----------- configuration 2-----#
     b1 = (0,1)
@@ -50,6 +55,9 @@ elif config == '2':
     b3 = (0,1)
     pi1 = 0.3
     pi2 = 0.7
+    func = lin_func
+    BL = -10
+    BR = 10
 elif config == '3':
     #----------- configuration 3-----#
     b1 = (3,-1)
@@ -57,22 +65,54 @@ elif config == '3':
     b3 = (-1,0.5)
     pi1 = 0.3
     pi2 = 0.3
+    func = lin_func
+    BL = -10
+    BR = 10
+elif config == '4':
+    #----------- configuration 4-----# 
+    b1 = (0.5,2)
+    b2 = (1,2.5)
+    b3 = (0,0)
+    pi1 = 0.5
+    pi2 = 0.5
+    func = poly_func
+    BL = -10
+    BR = 10
+elif config == '5':
+    #----------- configuration 5-----#
+    b1 = (-0.5,1)
+    b2 = (-1.5,1.5)
+    b3 = (0,0)
+    pi1 = 0.5
+    pi2 = 0.5
+    func = exp_func
+    BL = -10
+    BR = 10
+elif config == '6':
+    #----------- configuration 5-----#
+    b1 = (-0.5,1)
+    b2 = (-1.5,1.5)
+    b3 = (0,0)
+    pi1 = 0.5
+    pi2 = 0.5
+    func = exp_func
+    BL = -10
+    BR = 10
 else:
     sys.exit("Wrong configuration number!")
 
-# range of regression coefficients
-BL = -10
-BR = 10
-iter = 50
 
+
+iter = 50 # iterations of NPMLE_FW
 fname = str(b1[0]) + '_'+ str(b1[1])+'_'+ str(b2[0]) \
 +'_' +str(b2[1])+'_'+str(int(100*pi1)) +'percent'
 
-np.random.seed(626)
+
 
 #-----------------------------------------------------------#
 # generate simulated dataset
-X,y,C = generate_test_data(n,iter, b1, b2, b3,pi1,pi2,sigma)
+np.random.seed(626)
+X,y,C = generate_test_data(n,iter, b1, b2, b3,pi1,pi2,sigma,func)
 #-----------------------------------------------------------#
 
 
@@ -100,7 +140,7 @@ print("sigma:{},sigma_cv:{}".format(sigma,sigma_cv))
 
 #------------------------------------------------------------#  
 # paramters of b's and pi's are only for plotting purposes
-test(X, y,C, n,iter, b1, b2, b3,pi1,pi2,sigma,sigma_cv,-10,10)
+test(X, y,C, n,iter, b1, b2, b3,pi1,pi2,sigma,sigma_cv,-10,10,func)
 #------------------------------------------------------------#    
 
 #--------------------density plots--------------------------#
@@ -117,8 +157,8 @@ f1, B1, alpha1, L_rec1, L_final1 = NPMLE_FW(X,y,iter,sigma,BL,BR)
 f2, B2, alpha2, L_rec2, L_final2 = NPMLE_FW(X,y,iter,sigma_cv,BL,BR)
     
 #-------------------------------------------------------------#
-# EM algorithms need to specify the number of 
-k = int(float(config)/3) + 2
+# EM algorithms need to specify the number of vomponents
+num_component = int(float(config)/3) + 2
 #-------------------------------------------------------------#
 
 iter_EM = 100 # EM needs more iterations
@@ -126,11 +166,12 @@ iter_EM = 100 # EM needs more iterations
 #    
 #-------------------3: EMA with known sigma------------------#
 # needs sigma as input
-f3, B3, alpha3, sigma_array3, L_rec3, L_final3 = EMA_sigma(X,y,k,iter_EM,BL,BR,sigma)
+f3, B3, alpha3, sigma_array3, L_rec3, L_final3 = EMA_sigma(X,y,num_component,iter_EM,BL,BR,sigma)
 
 
 #------------------------------------------------------------#    
 #    
+
 #-------------------4: EMA_sigma without knowing sigma----------------#
 
 # need specification on the range of sigma
@@ -138,7 +179,10 @@ sigmaL =  0.1# = sigma_min
 sigmaR = np.sqrt(stats.variance(np.reshape(y, (len(y),)))) # = sigma_max
 # sigma_min sigma_max are also used in the cross-validation procedure
 
-f4, B4, alpha4, sigma_array4, L_rec4, L_final4 = EMA(X,y,k,iter_EM,BL,BR,sigmaL,sigmaR)
+# set up a random seed for EM 
+# because EM does not converge from time to time
+np.random.seed(626)
+f4, B4, alpha4, sigma_array4, L_rec4, L_final4 = EMA(X,y,num_component,iter_EM,BL,BR,sigmaL,sigmaR)
 
 #-----------------------------------#
 #
@@ -167,16 +211,17 @@ for i in range(len(x_list)):
     
     plt.subplot(1,len(x_list),i+1)
     
-    plt.plot(y, sum(alpha4[i]*scipy.stats.norm.pdf( y-(B4[0,i]+B4[1,i]*x), 0, sigma_array4[i]) \
-    for i in range(len(alpha4))),color = 'tab:red',label = 'EM',linestyle = line_styles[3])
+    if func.__name__ == 'lin_func':
+        plt.plot(y, sum(alpha4[i]*scipy.stats.norm.pdf( y-func(B4[:,i],[1,x]), 0, sigma_array4[i]) \
+               for i in range(len(alpha4))),color = 'tab:red',label = 'EM',linestyle = line_styles[3])
     
-    plt.plot(y,pi1*scipy.stats.norm.pdf(y - (b1[0]+b1[1]*x), 0, sigma)+pi2*scipy.stats.norm.pdf(y-(b2[0]+b2[1]*x),0, sigma)+\
-    (1-pi1-pi2)*scipy.stats.norm.pdf(y-(b3[0]+b3[1]*x),0, sigma),color = 'tab:blue',label = 'Truth',linestyle =line_styles[0])
+    plt.plot(y,pi1*scipy.stats.norm.pdf(y - func(b1,[1,x]), 0, sigma)+pi2*scipy.stats.norm.pdf(y-func(b2,[1,x]),0, sigma)+\
+    (1-pi1-pi2)*scipy.stats.norm.pdf(y-func(b3,[1,x]),0, sigma),color = 'tab:blue',label = 'Truth',linestyle =line_styles[0])
 
-    plt.plot(y, sum(alpha2[i]*scipy.stats.norm.pdf( y-(B2[0,i]+B2[1,i]*x), 0, sigma_cv) \
+    plt.plot(y, sum(alpha2[i]*scipy.stats.norm.pdf( y-func(B2[:,i],[1,x]), 0, sigma_cv) \
     for i in range(len(alpha2))),color = 'tab:green',label = 'NPMLE',linestyle = line_styles[1])
         
-    plt.plot(y, sum(alpha1[i]*scipy.stats.norm.pdf( y-(B1[0,i]+B1[1,i]*x), 0, sigma) \
+    plt.plot(y, sum(alpha1[i]*scipy.stats.norm.pdf( y-func(B1[:,i],[1,x]), 0, sigma) \
     for i in range(len(alpha1))),color = 'tab:orange',label = 'NPMLE_sigma',linestyle = line_styles[0])
     
     
@@ -193,10 +238,11 @@ for i in range(len(x_list)):
 custom_lines = [
             Line2D([0], [0], color= 'tab:blue', linestyle = line_styles[0]),
           Line2D([0], [0], color= 'tab:orange', linestyle = line_styles[0]),
-          Line2D([0], [0], color= 'tab:green', linestyle = line_styles[1]),
-          Line2D([0], [0], color= 'tab:red', linestyle = line_styles[3]),
+          Line2D([0], [0], color= 'tab:green', linestyle = line_styles[1])
           # Line2D([0], [0], color= 'tab:purple', linestyle = line_styles[2]),
     ]
+if func.__name__ == 'lin_func':
+    custom_lines.append( Line2D([0], [0], color= 'tab:red', linestyle = line_styles[3]))
 ax = plt.gca()
 lgd = ax.legend(custom_lines, ['Truth','NPMLE_sigma','NPMLE', 'EM',
                                 #'EM_sigma'
